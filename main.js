e33870aa07cb6343ee46850357a02264d54321cc
Martin <martingodec@hotmail.com>
2021-07-16 20:05:50 -0300

    Arreglo de error

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-function getData() {
-    console.log("IMPRIMIENDO")
-}
+const {
+    ipcRenderer
+} = require('electron')
 
-document.querySelector('.btnEd').addEventListener('click', () => {
-    getData()
-})
+const monto = document.querySelector('form');
+monto.addEventListener('submit', (e) => {
+    const total = document.querySelector('#monto').value;
+    console.log(total)
+    const totalImprimir = {
+        total: total
+    }
+ipcRenderer.send('total-imprimir', totalImprimir)
+
+    e.preventDefault();
+});
